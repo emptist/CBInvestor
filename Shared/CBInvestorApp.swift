03afac9cb661d6bab7c89ff0c50604c81d00e88f
emptist <jigme1968@gmail.com>
2021-08-31 09:51:48 +0800

    Initial Commit

---
+++ Shared/CBInvestorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CBInvestorApp.swift
+//  Shared
+//
+//  Created by jk on 2021/8/31.
+//
+
+import SwiftUI
+
+@main
+struct CBInvestorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by jk on 2021/8/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

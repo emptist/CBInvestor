66d5de3e1005c97ceac9e003e7d1badecf0659d9
emptist <jigme1968@gmail.com>
2021-09-05 00:46:12 +0800

    add error view

---
+++ Shared/Views/ErrorView.swift
@@ -0,0 +1,20 @@
+//
+//  ErrorView.swift
+//  CBInvestor (iOS)
+//
+//  Created by jk on 2021/9/5.
+//
+
+import SwiftUI
+
+struct ErrorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ErrorView_Previews: PreviewProvider {
+    static var previews: some View {
+        ErrorView()
+    }
+}

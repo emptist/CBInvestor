5d473cd1a0e52e045e415895d839fd43b767a738
emptist <jigme1968@gmail.com>
2021-09-05 02:34:59 +0800

    modified

--- Shared/Views/ErrorView.swift
+++ Shared/Views/ErrorView.swift
@@ -8,16 +8,49 @@
 import SwiftUI
 
 struct ErrorView: View {
+    
+    typealias ErrorViewActionHandler = () -> Void
+    
     let error: Error
+    let handler: ErrorViewActionHandler
+    internal init(error: Error, handler: @escaping ErrorView.ErrorViewActionHandler) {
+        self.error = error
+        self.handler = handler
+    }
+
     var body: some View {
-        Image(systemName: "exclamationmark.icloud.fill")
-            .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-            .font(.system(size: 50,weight: .heavy))
+        VStack {
+            Image(systemName: "exclamationmark.icloud.fill")
+                .foregroundColor(.gray)
+                .font(.system(size: 50,weight: .heavy))
+                //.padding(.bottom, 1)
+            Text("Ooops")
+                .foregroundColor(.black)
+                .font(.system(size: 20, weight: .heavy))
+            Text(error.localizedDescription)
+                .foregroundColor(.gray)
+                .font(.system(size: 15))
+                .multilineTextAlignment(.center)
+                .padding(.vertical,4)
+            Button(action: {
+                // some action here
+                handler()
+            }, label: {
+                Text("Retry")
+            })
+            .padding(.vertical,12)
+            .padding(.horizontal, 30)
+            .background(Color.blue)
+            .foregroundColor(.white)
+            .font(.system(size: 15, weight: .heavy))
+            .cornerRadius(10)
+        }
+        
     }
 }
 
 struct ErrorView_Previews: PreviewProvider {
     static var previews: some View {
-        ErrorView(error: APIError.decodingError)
+        ErrorView(error: APIError.decodingError) {}
     }
 }
--- Shared/Views/HomeView.swift
+++ Shared/Views/HomeView.swift
@@ -8,9 +8,26 @@
 import SwiftUI
 
 struct HomeView: View {
+    @StateObject var dataFromSite: DataFromSite = DataFromSite(service: JslDataService())
     var body: some View {
-        RowView(row:Row.dummyData)
-            .padding()
+        Group {
+            switch dataFromSite.state {
+            case .loading:
+                ProgressView()
+            case .failed(let error):
+                ErrorView(error: error, handler: dataFromSite.getDataRows)
+            case .success(let rows):
+                NavigationView {
+                    List(rows) { row in
+                        RowView(row: row)
+                    }
+                    .navigationTitle(Text("Rows"))
+                }
+                
+            }
+        }.onAppear(perform: dataFromSite.getDataRows)
+//        RowView(row:Row.dummyData)
+//            .padding()
     }
 }
 
